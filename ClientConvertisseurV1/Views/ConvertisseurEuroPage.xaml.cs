23ccbfc8c98e6864640da688249562030060ebd6
Léo Christophe <Leo.Christophe@etu.univ-savoie.fr>
2024-02-09 08:54:09 +0100

    Application MVC finit + documentation ConvertisseurEuroPage.xaml.cs

--- ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
+++ ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
@@ -8,10 +8,11 @@ using System;
 using System.ComponentModel;
 using System.Linq;
 using Microsoft.UI.Xaml;
+using System.Runtime.CompilerServices;
 
 namespace ClientConvertisseurV1.Views
 {
-    public sealed partial class ConvertisseurEuroPage : Page
+    public sealed partial class ConvertisseurEuroPage : Page, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -21,13 +22,14 @@ namespace ClientConvertisseurV1.Views
         private double euroAConvertir;
         private double resultatConverti;
 
+        /// <summary>
+        /// Constructeur de la window: initialisation des composants, définition du datacontext, récupération des données.
+        /// </summary>
         public ConvertisseurEuroPage()
         {
             this.InitializeComponent();
-            GetDataOnLoadAsync();
             this.DataContext = this;
-
-            
+            GetDataOnLoadAsync();
         }
 
         public ObservableCollection<Devise> ListeDevises
@@ -76,16 +78,20 @@ namespace ClientConvertisseurV1.Views
         {
             get
             {
-                return this.resultatConverti;
+                return resultatConverti;
             }
 
             set
             {
-                this.resultatConverti = value;
-                OnPropertyChanged(nameof(ResultatConverti)); // Notification de changement de propriété
+                resultatConverti = value;
+                OnPropertyChanged(nameof(ResultatConverti));
             }
         }
 
+        /// <summary>
+        /// Permet de notifier qu'une propriété a changé
+        /// </summary>
+        /// <param name="name">Nom de la propriété</param>
         protected void OnPropertyChanged(string name)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
@@ -95,6 +101,11 @@ namespace ClientConvertisseurV1.Views
             }
         }
 
+        /// <summary>
+        /// Fait apparaître un message dans une boîte de dialogue de manière asynchrone.
+        /// </summary>
+        /// <param name="v1">Contenu de la boîte de dialogue</param>
+        /// <param name="v2">Titre de la boîte de dialogue</param>
         private async void MessageAsync(string v1, string v2)
         {
             ContentDialog message = new ContentDialog
@@ -103,19 +114,26 @@ namespace ClientConvertisseurV1.Views
                 Content = v1,
                 CloseButtonText = "Ok"
             };
+            // on dit qu'on doit montrer la boîte de dialogue dans la fenêtre actuelle.
             message.XamlRoot = this.Content.XamlRoot;
             ContentDialogResult result = await message.ShowAsync();
         }
 
+        /// <summary>
+        /// Permet de récupérer les données de l'API de manière asynchrone.
+        /// </summary>
         private async void GetDataOnLoadAsync()
         {
+            // connexion à l'API
             WSService service = new WSService("http://localhost:44366/api/");
             List<Devise> result = await service.GetDevisesAsync("devises");
             if (result == null) { 
+                // erreur déclarée (API non dispo)
                 MessageAsync("Api non disponible !", "Erreur");
             }
             else
             { 
+                // on fait le binding
                 foreach(Devise devise in result)
                 {
                     this.ListeDevises.Add(devise);
@@ -124,12 +142,19 @@ namespace ClientConvertisseurV1.Views
             }
         }
 
+        /// <summary>
+        /// Gestion de l'évènement de clic sur  "Cliquer".
+        /// </summary>
+        /// <param name="sender">Bouton</param>
+        /// <param name="e">Evenement</param>
+        /// <exception cref="ArgumentNullException">Aucune monnaie n'a été choisie</exception>
         private void bt1_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
         {
-            if (this.MonnaieChoisi != null) { 
-                this.ResultatConverti = EuroAConvertir * this.MonnaieChoisi.Taux;
-            } 
-            else
+            if (this.MonnaieChoisi != null)
+            {
+                this.ResultatConverti = this.EuroAConvertir * this.MonnaieChoisi.Taux;
+            }
+            else 
             {
                 throw new ArgumentNullException("Le monnaie doit être choisie!");
             }
